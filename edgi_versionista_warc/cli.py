296c88551ae10c9bf651d7b5140206f8d4b7f440
Rob Brackett <rob@robbrackett.com>
2025-01-17 16:35:30 -0800

    Lint happier

--- edgi_versionista_warc/cli.py
+++ edgi_versionista_warc/cli.py
@@ -12,7 +12,10 @@ def cli() -> None:
     parser.add_argument('--uncompressed', action='store_true', help='Create uncompressed `.warc` files instead of gzipped `.warc.gz` files')
     parser.add_argument('--limit', type=int, help='Archive up to this many records from Web Monitoring DB')
     parser.add_argument('--size', type=float, default=7.95, help='Generate WARC up to about this many gigabytes each')
-    parser.add_argument('path', help='Path to generate WARC files at. If the path is `out/archive`, WARC files will be built at paths like `out/archive-YYYY-MM-DDThhmmss.warc.gz`')
+    parser.add_argument('path', help=(
+        'Path to generate WARC files at. If the path is `out/archive`, WARC files will be built at paths like'
+        '`out/archive-YYYY-MM-DDThhmmss.warc.gz`'
+    ))
     configuration = parser.parse_args()
 
     main(
