be960f7af22be2c034053c0da4e9a62e6a344469
Rob Brackett <rob@robbrackett.com>
2025-01-22 08:47:41 -0800

    Make CLI options for path dumber, they were too complex

--- edgi_versionista_warc/cli.py
+++ edgi_versionista_warc/cli.py
@@ -13,27 +13,24 @@ def cli() -> None:
     parser.add_argument('--size', type=float, default=7.95, help='Generate WARC up to about this many gigabytes each')
     parser.add_argument('--from', type=parse_timestamp, help='Start from this timestamp (ISO format)')
     parser.add_argument('--guess-status', action='store_true', help='Guess status code when it is missing from DB data')
+    parser.add_argument('--filename', default='edgi-wm-versionista', help=(
+        'Base name for the generated WARC files. Each file will be named '
+        '`<name>--<timestamp>.warc`'
+    ))
     parser.add_argument('path', help=(
-        'Path to generate WARC files at. If the path is `out/archive`, WARC files will be built at paths like'
-        '`out/archive-YYYY-MM-DDThhmmss.warc.gz`'
+        'Directory to generate WARC files in. Files will be built at paths like'
+        '`<path>/edgi-wm-versionista--YYYY-MM-DDThhmmss.warc.gz`'
     ))
     configuration = parser.parse_args()
 
     path = Path(configuration.path)
-    if path.is_dir():
-        path = path
-        name = 'edgi-wm-versionista'
-    else:
-        path = path.parent
-        name = path.name
-
-    path.mkdir(parents=True, exist_ok=True)
+    path.parent.mkdir(parents=True, exist_ok=True)
     logging.basicConfig(filename=str(path / 'log.txt'), level=logging.WARNING)
     logging.getLogger(__name__.split('.')[0]).setLevel(logging.INFO)
 
     main(
         path=str(path),
-        name=name,
+        name=configuration.filename,
         gzip=(not configuration.uncompressed),
         limit=configuration.limit,
         warc_size=int(configuration.size * GIGABYTE),
